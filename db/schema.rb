f21574a863dd52348a888440243ffcf08fd76a0c
Palma Toth <palma@PalMac.local>
2019-06-20 14:14:11 +0200

    move to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_20_100542) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
     t.text "body"
